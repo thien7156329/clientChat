3c9eb543e952f97678367b2a0ecc6faa7f17b66a
LTTHIEN\Administrator <le.trong.thien@quantic.com.vn>
2020-09-24 16:33:21 +0700

    hi

--- src/message-item.js
+++ src/message-item.js
@@ -11,23 +11,18 @@ export default class messageItem extends React.Component {
 
     isMine = () =>{
         const user = JSON.parse(localStorage.getItem("user"));
-        console.log(user)
         if(this.props.type == 0){
             if(this.props.user == user.name){
                 this.setState({
                     checkIsMine: 'is-mine'
                 })
             }
-            console.log(this.props.user)
-            console.log(this.props.type)
         }else if(this.props.type == 1){
             if(this.props.user == user.profileObj.name){
                 this.setState({
                     checkIsMine: 'is-mine'
                 })
             }
-            console.log(this.props.user)
-            console.log(this.props.type, '22222')
         }
     }
 
