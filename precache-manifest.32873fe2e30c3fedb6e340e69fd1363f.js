10dc010481f7dbf4ec6e8a0a664ea34eb43e63ba
LTTHIEN <le.trong.thien@quantic.com.vn>
2022-03-23 11:08:51 +0700

    Updates

---
+++ precache-manifest.32873fe2e30c3fedb6e340e69fd1363f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71ebd87fbbf8b8600c86",
+    "url": "/static/css/main.cb564b4b.chunk.css"
+  },
+  {
+    "revision": "71ebd87fbbf8b8600c86",
+    "url": "/static/js/main.2ee5a6c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3fe22ff110e52acf323",
+    "url": "/static/css/2.33268f13.chunk.css"
+  },
+  {
+    "revision": "f3fe22ff110e52acf323",
+    "url": "/static/js/2.566f80a5.chunk.js"
+  },
+  {
+    "revision": "dcf925699533fdcea98ef1522dcd96f7",
+    "url": "/index.html"
+  }
+];
